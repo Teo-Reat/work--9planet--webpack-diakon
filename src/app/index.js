91daf0e789cce0e177139eb91105131078ceed4f
Teo-Reat <teo.reat@gmail.com>
2020-07-07 16:01:54 +0300

    Small desctop version completed. Hovers and menu.

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,4 @@
 import $ from 'jquery'
-// import './formstyler'
 import 'slick-carousel'
 
 $(document).ready(() => {
@@ -25,13 +24,26 @@ $(document).ready(() => {
 	});
 
 	$('.news__actions__slider').slick({
-		autoplay: true,
-		dots: true,
-		arrows: false,
+		// autoplay: true,
+		dots: false,
+		arrows: true,
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-prev news__actions__slick-prev"><svg class="icon icon-arrow-l"><use xlink:href="#icon-arrow-l"></use></svg></button>',
+		nextArrow: '<button type="button" class="slick-next news__actions__slick-next"><svg class="icon icon-arrow-r"><use xlink:href="#icon-arrow-r"></use></svg></button>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			},
+		]
 	});
 
 	$('.js-header__menu__btn').click(() => {
@@ -39,6 +51,11 @@ $(document).ready(() => {
 		$('.js-header__menu__btn').toggleClass('header__menu__btn_close')
 	})
 
+	$('.js-header__search__button').click(() => {
+		$('.js-search').slideDown()
+	})
 
-	// $('#network__select').styler()
+	$('.js-search__form__close').click(() => {
+		$('.js-search').slideUp()
+	})
 })
